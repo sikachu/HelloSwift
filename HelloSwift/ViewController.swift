40f07f0823b809654a919e3fc82245bb39633ae0
Prem Sichanugrist <s@sikac.hu>
2014-06-03 10:23:48 -0400

    Initial Commit

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var label : UILabel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Hello Swift"
     }
 
     override func didReceiveMemoryWarning() {
